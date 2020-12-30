52a7a46ef877f16f123e8dffa9b419c52f96a47b
Hoon Kim <hoonsubin@icloud.com>
2020-12-29 23:02:39 -0800

    boilerplate work for making a new page

--- packages/apps-routing/src/index.ts
+++ packages/apps-routing/src/index.ts
@@ -11,6 +11,7 @@ import calendar from './calendar';
 import claims from './claims';
 import contracts from './contracts';
 import council from './council';
+import customSignature from './custom-signature';
 import democracy from './democracy';
 import explorer from './explorer';
 import extrinsics from './extrinsics';
@@ -35,6 +36,7 @@ export default function create (t: TFunction): Routes {
     addresses(t),
     explorer(t),
     claims(t),
+    customSignature(t),
     poll(t),
     transfer(t),
     genericAsset(t),
---
+++ packages/page-custom-signature/src/index.tsx
@@ -0,0 +1,74 @@
+// Copyright 2017-2020 @polkadot/app-js authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { AppProps as Props } from '@polkadot/react-components/types';
+
+import React, { useRef } from 'react';
+import { Route, Switch } from 'react-router';
+
+import { Icon, Tabs } from '@polkadot/react-components';
+import { useSudo } from '@polkadot/react-hooks';
+
+import SetKey from './SetKey';
+import Sudo from './Sudo';
+import { useTranslation } from './translate';
+
+// NOTE: this is a copy of the SUDO module page. This will gradually change later on.
+
+function CustomSignatureApp ({ basePath }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const { allAccounts, isMine, sudoKey } = useSudo();
+
+  const itemsRef = useRef([
+    {
+      isRoot: true,
+      name: 'index',
+      text: t<string>('Sign Transaction')
+    },
+    {
+      name: 'key',
+      text: t<string>('Set sudo key')
+    }
+  ]);
+
+  return (
+    <main>
+      <header>
+        <Tabs
+          basePath={basePath}
+          items={itemsRef.current}
+        />
+      </header>
+      {isMine
+        ? (
+          <Switch>
+            <Route path={`${basePath}/key`}>
+              <SetKey
+                allAccounts={allAccounts}
+                isMine={isMine}
+                sudoKey={sudoKey}
+              />
+            </Route>
+            <Route>
+              <Sudo
+                allAccounts={allAccounts}
+                isMine={isMine}
+                sudoKey={sudoKey}
+              />
+            </Route>
+          </Switch>
+        )
+        : (
+          <article className='error padded'>
+            <div>
+              <Icon icon='ban' />
+              {t<string>('You do not have access to the current sudo key')}
+            </div>
+          </article>
+        )
+      }
+    </main>
+  );
+}
+
+export default React.memo(CustomSignatureApp);
